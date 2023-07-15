57730160207df7a6b45319ba9c8a7a1effa3c0df
ben40d9 <benpaley6@gmail.com>
2023-07-15 03:54:35 -0700

    uses the ConversationalRetrievalQAChain for a streaming application. need to replace "Your entire text or document goes here" with the actual comment data

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,41 @@
+// import { StreamingTextResponse, LangChainStream, Message } from 'ai'
+// import { ChatOpenAI } from 'langchain/chat_models/openai'
+// import { AIChatMessage, HumanChatMessage } from 'langchain/schema'
+
+// export const runtime = 'edge'
+
+// export async function POST(req: Request) {
+//   const { messages } = await req.json()
+
+//   const { stream, handlers } = LangChainStream()
+
+//   const llm = new ChatOpenAI({
+//     streaming: true
+//   })
+
+//   llm
+//     .call(
+//       (messages as Message[]).map(m =>
+//         m.role == 'user'
+//           ? new HumanChatMessage(m.content)
+//           : new AIChatMessage(m.content)
+//       ),
+//       {},
+//       [handlers]
+//     )
+//     .catch(console.error)
+
+//   return new StreamingTextResponse(stream)
+// }
+
 import { StreamingTextResponse, LangChainStream, Message } from 'ai'
 import { ChatOpenAI } from 'langchain/chat_models/openai'
-import { AIChatMessage, HumanChatMessage } from 'langchain/schema'
+import { AIMessage, HumanMessage } from 'langchain/schema'
+import { ConversationalRetrievalQAChain } from "langchain/chains";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { BufferMemory } from "langchain/memory";
 
 export const runtime = 'edge'
 
@@ -11,17 +46,33 @@ export async function POST(req: Request) {
 
   const llm = new ChatOpenAI({
     streaming: true
-  })
+  });
 
-  llm
+  //replace with the actual comment data
+  const text = "Your entire text or document goes here";
+  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
+  const docs = await textSplitter.createDocuments([text]);
+  const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+
+  const chain = ConversationalRetrievalQAChain.fromLLM(
+    llm,
+    vectorStore.asRetriever(),
+    {
+      memory: new BufferMemory({
+        memoryKey: "chat_history"
+      })
+    }
+  );
+
+  chain
     .call(
       (messages as Message[]).map(m =>
         m.role == 'user'
-          ? new HumanChatMessage(m.content)
-          : new AIChatMessage(m.content)
+          ? new HumanMessage(m.content)
+          : new AIMessage(m.content)
       ),
       {},
-      [handlers]
+      
     )
     .catch(console.error)
 
